aa1c6bf54e4b64ecf79d31e91c6f2c2f5dcf048e
21edclique <bryanjinez200027@gmail.com>
2024-10-04 15:37:35 -0500

    Add new  handleMessages

--- chaskipass-backend/src/routes/clients.routes.ts
+++ chaskipass-backend/src/routes/clients.routes.ts
@@ -1,7 +1,10 @@
 import { getClients } from './../controllers/clients.controllers';
+import { createClient} from './../controllers/clients.controllers';
 import { Router } from "express";
 import protectRoute from '../middleware/protectRoute.middleware';
 
 const router=Router();
 router.get('/',getClients);
+router.post('/newClient',createClient);
+
 export default router;
